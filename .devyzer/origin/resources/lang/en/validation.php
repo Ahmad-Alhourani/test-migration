683f2abc3433602517dd11fbb87d79e7695a77b7
Ahmad <ahmad.alhourani@xlabtechs.com>
2019-02-03 22:31:27 +0200

    Test

--- .devyzer/origin/resources/lang/en/validation.php
+++ .devyzer/origin/resources/lang/en/validation.php
@@ -193,8 +193,19 @@ return [
                     'timezone' => 'Timezone',
                     'language' => 'Language'
                 ]
-            ]
+            ],
 
+            //start_Test_start
+            'tests' => [
+                'id' => "Id",
+                'name' => "Name",
+                'name5' => "Name5",
+                'l_name' => "Last Name",
+                'email' => "Email",
+                'des' => "Des",
+                'sms' => "SMS"
+            ]
+            //end_Test_end
             // Do not delete me :) I'm used for auto-generation
         ],
 
